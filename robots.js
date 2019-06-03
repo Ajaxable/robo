0d0795381f16353bc0cc286141aafc7e389f101a
Ajaxable <43863337+Ajaxable@users.noreply.github.com>
2019-06-03 10:31:54 +0200

    Add files via upload

---
+++ robots.js
@@ -0,0 +1,44 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Marvin Brown",
+    username: "Marv",
+    email: "MB@poomail.com"
+  },
+  {
+    id: 2,
+    name: "Fanny Pack",
+    username: "Fan",
+    email: "FP@fanmail.com"
+  },
+  {
+    id: 3,
+    name: "Mark Blue",
+    username: "Blue",
+    email: "mark@legend.com"
+  },
+  {
+    id: 4,
+    name: "Sandy Crack",
+    username: "SandyC",
+    email: "SC@smail.com"
+  },
+  {
+    id: 5,
+    name: "Jay Bird",
+    username: "Birdy",
+    email: "jib@jmail.com"
+  },
+  {
+    id: 6,
+    name: "Brad Dowl",
+    username: "Dowl",
+    email: "bd@email.com"
+  },
+  {
+    id: 7,
+    name: "Windy Road",
+    username: "Road",
+    email: "road@gmail.com"
+  }
+];
